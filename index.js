47cfc899df6f92741c85df236f562bde14e10f65
Geir Gåsodden <geir.gasodden@gmail.com>
2023-01-24 07:07:56 +0100

    Adds function for adding days (minor)

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ async function getPrices (url) {
   try {
     const response = await fetch(url)
     if (!response.ok) {
-        console.error('Response not OK', response.status)
-        return []  
+      console.error('Response not OK', response.status)
+      return []
     } else {
-        const json = await response.json()
-        return json
+      const json = await response.json()
+      return json
     }
   } catch (error) {
     console.error(error)
@@ -14,14 +14,21 @@ async function getPrices (url) {
   }
 }
 
-function generateUrl(dato = new Date(), zone = 'NO2') {
-    const apiUrl = 'https://www.hvakosterstrommen.no/api/v1/prices'
-    const datestring = dato.toISOString().substring(0, 10)
-    const [year, month, day] = datestring.split('-')
-    return `${apiUrl}/${year}/${month}-${day}_${zone}.json`
+function generateUrl (dato = new Date().toISOString(), zone = 'NO2') {
+  const apiUrl = 'https://www.hvakosterstrommen.no/api/v1/prices'
+  const datestring = new Date(dato).toISOString().substring(0, 10)
+  const [year, month, day] = datestring.split('-')
+  return `${apiUrl}/${year}/${month}-${day}_${zone}.json`
+}
+
+function addDays (dateString, numberOfDays) {
+  const copyOfDate = new Date(dateString)
+  copyOfDate.setDate(copyOfDate.getDate() + numberOfDays)
+  return copyOfDate
 }
 
 export {
   getPrices,
-  generateUrl
+  generateUrl,
+  addDays
 }
